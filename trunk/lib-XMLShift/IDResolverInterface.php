538a13b27c75e2d73ec01e32a26add1e334f06ba
ttcremers <ttcremers@b7300cca-1d4d-0410-9fc4-07b5614f54de>
2008-05-16 11:41:36 +0000

    add phpdoc

--- trunk/lib-XMLShift/IDResolverInterface.php
+++ trunk/lib-XMLShift/IDResolverInterface.php
@@ -1,6 +1,17 @@
 <?php
+/**
+ * @author ttcremers@gmail.com
+ * @copyright Lunatech Research B.V 2008
+ * IDResolverInterface when implementing your own id resolver it should implement this interface.
+ */
 interface IDResolverInterface {
-	// Should return Object found with id
+	/**
+	 * Should return Object found with id
+	 * 
+	 * @param $id The id found in the xml
+	 * @param string $className Can be any classname but usually the classname that was annotated  
+	 * @return object 
+	 */ 
 	function resolve($id, $className);
 }
 ?>
--- trunk/lib-XMLShift/XMLShiftException.php
+++ trunk/lib-XMLShift/XMLShiftException.php
@@ -1,3 +1,8 @@
 <?php
+/**
+ * @author ttcremers@gmail.com
+ * @copyright Lunatech Research B.V. 2008
+ * Exception class for XMLShift exceptions. This class doesn't implement anything yet.
+ */
 class XMLShiftException extends Exception {}
 ?>
