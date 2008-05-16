f3ffa94698a07f829731e2a42902c051e7fe48e2
ttcremers <ttcremers@b7300cca-1d4d-0410-9fc4-07b5614f54de>
2008-05-16 12:55:21 +0000

    Added documentation

--- phpREST-Skeleton/proxy.php
+++ phpREST-Skeleton/proxy.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @author ttcremers@gmail.com
+ * @copyright Lunatech Research B.V.
+ * @package phpREST-Skeleton
+ * 
+ * The applications proxy. You can add application specific setup here. 
+ * When using apache this is the script to which you refer in your rewrite rule.
+ */
 define("BASEPATH", '../');
 require_once '../lib-phpREST/RESTProxy.php';
 ?>
