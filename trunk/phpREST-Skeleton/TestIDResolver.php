f505a20326f09a7699d1e6318db6808e6e9ed8e1
ttcremers <ttcremers@b7300cca-1d4d-0410-9fc4-07b5614f54de>
2008-05-16 12:55:21 +0000

    Added documentation

--- trunk/phpREST-Skeleton/TestIDResolver.php
+++ trunk/phpREST-Skeleton/TestIDResolver.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @author ttcremers@gmail.com
+ * @copyright Lunatech Research B.V. 2008
+ * @package phpREST-Skeleton
+ */
 require_once 'IDResolverInterface.php';
 class TestIDResolver implements IDResolverInterface {
 	public function resolve($id, $className) {
--- trunk/phpREST-Skeleton/resources/demo.php
+++ trunk/phpREST-Skeleton/resources/demo.php
@@ -1,7 +1,13 @@
 <?php
-/*
- * Created on Apr 28, 2008
- * 
+/**
+ * @author ttcremers@gmail.com
+ * @copyright Lunatech Research B.V.
+ * @package phpREST-Skeleton 
+ */
+require_once 'CoreResource.php';
+require_once 'demo.php';
+
+/** 
  * This is an example resource class.
  * 
  * Anatomy of an phpREST Resource
@@ -11,26 +17,43 @@
  * 2. Resource classes use poor-mans namespaces 
  * 3. Resource classes always implement ALL the CRUD methods
  * 4. Resource classes inherit a couple of handy properties
- *    - $this->id, if there was an id in the url this is it
- *    - $this->request, for unexpected needs
- *    - $this->serviceConfig, access to the service configuration
+ *    $this->id if there was an id in the url this is it,
+ *    $this->request for unexpected needs,
+ *    $this->serviceConfig access to the service configuration
  * 
+ * @package phpREST-Skeleton
+ * @example phpREST-Skeleton/resources/demo.php
+ * @todo The Response object and the generic object should be class properties 
  */
-require_once 'CoreResource.php';
-require_once 'demo.php';
-
 class resources_Demo extends CoreResource {
 	
-	// HTTP POST
+	/**
+	 * Gets called on a HTTP POST request on the resource
+	 *
+	 * @param object $object Object associated with this resource. Initialised by the adapter
+	 * @param Response $response 
+	 * @see lib-phpREST::AdapterInterface
+	 */
 	public function create($object, Response $response) {
 		$response->statusCode=200;
 		$response->body="Hello, ".$object->name." your age is: ". $object->age. " and a friendly message of a relative object: ".$object->refs[0]->message;
 	}
 	
-	// HTTP PUT
+	/**
+	 * Gets called on a HTTP PUT request on this resource
+	 *
+	 * @param object $object Object associated with this resource. Initialised by the adapter
+	 * @param Response $response 
+	 */
 	public function update($object, Response $response) {}
 	
-	// HTTP GET 
+	/**
+	 * Gets called on a HTTP GET request on this resource
+	 *
+	 * @param Response $response
+	 * @return object Object to be marshalled to the right content-type by the adapter
+	 * @see lib-phpREST::AdapterInterface
+	 */ 
 	public function select(Response $response) {
 		$response->statusCode=200;
 		$demo = new Demo();
@@ -39,7 +62,11 @@ class resources_Demo extends CoreResource {
 		return $demo;
 	}
 	
-	// HTTP DELETE
+	/**
+	 * Gets called on a HTTP DELETE request on this resource.
+	 *
+	 * @param Response $response
+	 */
 	public function delete(Response $response) {}
 }
 ?>
