e7d9182d077032ff97b6e3f27a35219518b1b33c
Targeter <Targeter@b7300cca-1d4d-0410-9fc4-07b5614f54de>
2008-05-27 11:44:11 +0000

    now also loading IdResolver for bodyWrite()

--- trunk/lib-phpREST/XMLAdapter.php
+++ trunk/lib-phpREST/XMLAdapter.php
@@ -12,35 +12,39 @@ require_once 'CoreXMLShift.php';
  * @package phpREST
  */
 class XMLAdapter implements AdapterInterface {
-	
+
 	public function bodyRead(Request $request, RESTServiceConfig $serviceConfig) {
 		try {
 			$xmlShift = new CoreXMLShift();
-			
-			// Load the IDResolver if any
-			$xmlIDResolverClass = $serviceConfig->adapterSection['xml-idresolver-class'];
-			if ($xmlIDResolverClass) {
-				$object = CoreUtil::loadCoreClass($xmlIDResolverClass);
-				if (is_object($object))
-					$xmlShift->setIDResolver($object);
-			}
+			$this->loadIdResolver($xmlShift, $serviceConfig);
 			return $xmlShift->unMarshall($request->body);
 		} catch (Exception $e) {
 			throw new RESTException($e->getMessage(), 500);
 		}
 		return null;
 	}
-	
+
 	public function bodyWrite($contentObjectRep, Response $response, RESTServiceConfig $serviceConfig) {
 		try {
 			$classNamespace = $serviceConfig->adapterSection['xml-class-namespace'];
 			$xmlShift = new CoreXMLShift($classNamespace ? $classNamespace : null);
+			$this->loadIdResolver($xmlShift, $serviceConfig);
 			$response->body=$xmlShift->marshall($contentObjectRep);
 			return true;
 		} catch (XMLShiftException $e) {
 			throw new RESTException($e->getMessage(), 500);
 		}
 		return false;
+	}
+
+	private function loadIdResolver(CoreXMLShift $xmlShift, RESTServiceConfig $serviceConfig){
+		// Load the IDResolver if any
+		$xmlIDResolverClass = $serviceConfig->adapterSection['xml-idresolver-class'];
+		if ($xmlIDResolverClass) {
+			$object = CoreUtil::loadCoreClass($xmlIDResolverClass);
+			if (is_object($object))
+			$xmlShift->setIDResolver($object);
+		}
 	}
 }
 ?>
