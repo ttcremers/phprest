0e5cbe12166675aaa703fb8630138938e4f8b57b
Targeter <Targeter@b7300cca-1d4d-0410-9fc4-07b5614f54de>
2008-05-29 14:34:16 +0000

    Now allowing non-alphanumeric annotation values, to allow for xml namespaces

--- trunk/lib-ReflectionAnnotate/CoreAnnotation.php
+++ trunk/lib-ReflectionAnnotate/CoreAnnotation.php
@@ -46,8 +46,9 @@ class ReflectionAnnotate_CoreAnnotation {
 	 */
 	protected function extractValueForAnnotation($key, $comment) {
 		$matches = array();
-		$regex = "/".constant('ReflectionAnnotate_CoreAnnotation::TAG_PREFIX').$key.":\s*(\w*)(\r\n|\r|\n)/U";
+		$regex = "/".constant('ReflectionAnnotate_CoreAnnotation::TAG_PREFIX').$key.":\s*(\S*)(\r\n|\r|\n)/U";
 		preg_match($regex, $comment, $matches);
+		error_log("{$key}:{$matches[1]}");
 		if ($matches[1]) 
 			return trim($matches[1]);
 	}
