ce2bc090127abf397cfc0439870912cb8055f2ea
Targeter <Targeter@b7300cca-1d4d-0410-9fc4-07b5614f54de>
2008-06-10 08:18:47 +0000

    updated phpDoc for IDResolver interface

--- lib-XMLShift/IDResolver.php
+++ lib-XMLShift/IDResolver.php
@@ -8,31 +8,39 @@
  * IDResolver when implementing your own id resolver it should implement this interface.
  * @package XMLShift
  * @example phpREST-Skeleton/TestIDResolver.php
+ * 
+ * //TODO rename URL in method to URI
  */
 interface IDResolver {
 	/**
-	 * Should return Object found with id
-	 * 
+	 * Should return Object of type $className with the given $id. 
+	 *  
 	 * @param $id The id found in the xml
 	 * @param string $className Can be any classname but usually the classname that was annotated  
 	 * @return object, or null if not found 
 	 */ 
 	function resolve($id, $className);
 	
-	function reverse($id);
+	/**
+	 * Returns the ID for the given Object.
+	 *
+	 * @param object $object
+	 * @return int, the ID for the given object
+	 */
+	function reverse($object);
 	
 	/**
-	 * Should return Object found with id
+	 * Should return Object found at the given URI 
 	 * 
-	 * @param url the URL to retrieve an object for.  
+	 * @param string uri the URI to retrieve an object for.  
 	 * @return object 
 	 */ 
-	function resolveURL($url);
+	function resolveURL($uri);
 	
 	/**
 	 * Construct a URL to the given object
 	 * 
-	 * @param url the URL to retrieve an object for
+	 * @param object $object the object to construct an URI to
 	 * @return string the URL to the given object
 	 */
 	function constructURL($object);
