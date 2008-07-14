41ecdce7616ec6631d052ea17f888464698af5b2
Targeter <Targeter@b7300cca-1d4d-0410-9fc4-07b5614f54de>
2008-07-14 14:32:28 +0000

    Added filtering for forwarding

--- trunk/lib-phpREST/StompForwarder.php
+++ trunk/lib-phpREST/StompForwarder.php
@@ -16,11 +16,16 @@ class StompForwarder implements Observer{
 	public function StompForwarder(RESTServiceConfig $serviceConfig){
 		$this->brokerUrl = $serviceConfig->observerSection["broker-url"];
 		$this->queue = $serviceConfig->observerSection["queue"];
+		$this->validResources = explode(' ', trim($serviceConfig->observerSection["resources"]));
 		$this->shift = new CoreXMLShift();
 		$this->shift->setIDResolver($this->loadIdResolver($serviceConfig));
 	}
 
 	public function notify($action, $value, $resource = null){
+		if(!is_null($resource) && is_array($this->validResources) && array_search($resource, $this->validResources) === FALSE){
+			// resource given but not in $validResources, do nothing.
+			return;
+		}
 
 		if(is_object($value)){
 			$message = $this->shift->marshall($value);
@@ -28,7 +33,6 @@ class StompForwarder implements Observer{
 			$message = $value;
 		}
 
-		error_log("FORWARD: {$message}");
 		if($action == "CREATE" | $action == "UPDATE"){
 			$stomp = new Stomp($this->brokerUrl);
 			$stomp->connect();
