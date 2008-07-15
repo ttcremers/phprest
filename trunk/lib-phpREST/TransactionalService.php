e649d29d995de8cc9f79e9fc9a1783a7b877ba13
Targeter <Targeter@b7300cca-1d4d-0410-9fc4-07b5614f54de>
2008-07-15 07:34:57 +0000

    Moved TransactionalService to framework

---
+++ trunk/lib-phpREST/TransactionalService.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'CoreService.php';
+
+class TransactionalService extends CoreService  {
+
+	public function service(Request $request, Response $response, AdapterInterface $contentAdapter, RESTServiceConfig $serviceConfig) {
+		$con = Propel::getConnection();
+		$con->beginTransaction();
+		try{
+			parent::service($request, $response, $contentAdapter, $serviceConfig);
+			$con->commit();
+		}catch (Exception $e){
+			error_log("Exception caught, rolling back. $e->getMessage()");
+			$con->rollBack();
+			throw $e;
+		}
+	}
+
+}
+?>
